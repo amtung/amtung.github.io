eaded9e5576970532a0dc0c7034cdab0954a6643
Annie <annie.mmt@gmail.com>
2017-04-12 10:50:55 -0400

    added app photos into portfolio

--- app.js
+++ app.js
@@ -48,16 +48,18 @@ var Home = React.createClass({
 	render: function(){
 		return(
 			<div>
-			<div className="laptop-img"> <img className="images" src="Image/laptop-shadow.png"/></div>
-			<div className="aboutContainer" id="section"><p><br/>Hi, I’m Annie. I am an iOS developer with a passion for art, music and technology. Born in Hong Kong, moved to New York at age 12, I’ve always been adventurous and adaptive to change. Graduated from University at Albany with a Bachelor’s degree in Communication and minor in Japanese. My background includes executing digital marketing strategies, managing operation in start-ups, and a successful track record in sales. Being exposed to the tech industry made me realize that I wanted to be a part of it, the possibility of combining art and technology is what initially drew me into this field. The power of building with code is what excites me and continues to inspire me everyday. Aside from coding, I enjoy doing arts and crafts, exploring museums and listening to Electronic dance music, most of all I like the small joys that a simple life brings.</p></div>
+			<div className="laptop-img"> <img className="images" src="Image/laptop-border.jpg"/></div>
+			<div className="aboutContainer" id="section"><p><br/>Hi, my name is Annie. I am an iOS developer with a passion for art, music and technology. Born in Hong Kong, moved to New York at age 12, I’ve always been adventurous and adaptive to change. Graduated from University at Albany with a Bachelor’s degree in Communication and minor in Japanese. My background includes executing digital marketing strategies, managing operation in start-ups, and a successful track record in sales. <br/><br/>The possibility of combining art and technology is what initially drew me into this field, the power of building apps from scratch is what excites me and continues to inspire me everyday. Aside from coding, I enjoy doing arts and crafts, exploring museums and listening to Electronic dance music, most of all I like the small joys that a simple life brings.</p></div>
 			<div className="contactInfo"><br/>
 				<a href="https://github.com/amtung"><img className="icons" src="Image/github.png"/></a>
 				<a href="https://www.linkedin.com/in/amtung/"><img className="icons" src="Image/linkedin.png"/></a>
 				<a href="mailto:annie.mmt@gmail.com"><img className="icons" src="Image/gmail.png"/></a>
 				<a href="https://twitter.com/annietungg"><img className="icons" src="Image/twitter.png"/></a>
 				<a href="https://medium.com/@amtung"><img className="icons" src="Image/medium.png"/></a>
+				<div className="signature"> <img className="images" src="image/signature.png"/></div>
 			</div>
-			</div> 
+			</div>
+
 		)
 	}
 });
@@ -72,7 +74,11 @@ var Work = React.createClass({
 	render: function(){
 		return(
 			<div>
-				<div className="workContainer">Portfolio here.</div>
+				<div className="myApps">
+					<a href="https://github.com/C4Q/AC3.2-Kagami"><img className="appPhoto" src="Image/kagami-mirror.png"/></a>
+					<a href="https://github.com/C4Q/AC3.2-groupThree-ImmigrantServices"><img className="appPhoto" src="Image/Immiguide.png"/></a>
+					<a href="https://github.com/C4Q/AC3.2-Cheers"><img className="appPhoto" src="Image/Cheers.png"/></a>
+				</div>
 			</div>
 		)
 	}
